4cd50704898521e9a1cd399b8d65d874c8456f4b
maxim vladimirovich <m.shepelbain@rambler.ru>
2018-10-24 16:17:08 +0300

    My lesson 2 c#

---
+++ Lesson_2/Lesson_2/Fixed_fee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    class Fixed_fee : Worker//Фиксированная оплата
+    {
+        public Fixed_fee(string name, decimal salary, decimal baserate) : base(name, salary, baserate)
+        {
+
+        }
+        public override void Calculate()
+        {
+            Averge_Salary *= BaseRate;
+        }
+    }
+}
+ 
